685bc0f9644e4bd8ba02e822b7182e8d9c8c1cbd
matus <bartko.mat@gmail.com>
2018-09-23 14:14:58 +0200

    stacks renamed

--- cloudformation/create-microservice-pipeline.sh
+++ cloudformation/create-microservice-pipeline.sh
@@ -3,6 +3,6 @@
 aws cloudformation create-stack \
     --capabilities CAPABILITY_NAMED_IAM \
     --region eu-west-1 \
-    --stack-name wixit-auth-infrastructure-pipeline \
+    --stack-name wixit-auth-infrastructure \
     --template-body file://microservice-pipeline.yaml \
     --parameters file://microservice-pipeline-params.json
