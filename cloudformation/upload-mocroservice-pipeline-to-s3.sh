35f04620c9bfac85a6c0550ed1fbed33585589a9
matus <bartko.mat@gmail.com>
2018-09-30 01:11:47 +0200

    naming changed

--- cloudformation/upload-mocroservice-pipeline-to-s3.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-aws s3 cp microservice-pipeline.yaml s3://wixit-auth-webhooks-cloudformation-templates
-aws s3 cp microservice-pipeline-params-without-variables.json s3://wixit-auth-webhooks-cloudformation-templates
